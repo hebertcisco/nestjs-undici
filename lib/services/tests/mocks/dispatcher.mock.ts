b2eace69baa441fc1cbe3c453b32c1aa48deec23
Favela <faveladoweb@gmail.com>
2022-11-17 17:11:02 +0000

    fix() The return value of "toString" must be used

--- lib/services/tests/mocks/dispatcher.mock.ts
+++ lib/services/tests/mocks/dispatcher.mock.ts
@@ -70,84 +70,63 @@ export const dispatcherMock: Dispatcher = {
     eventName: string | symbol,
     listener: (...args: any[]) => void,
   ): Dispatcher => {
-    eventName.toString();
-    listener.name.toString();
     return this;
   },
   on: (
     eventName: string | symbol,
     listener: (...args: any[]) => void,
   ): Dispatcher => {
-    eventName.toString();
-    listener.name.toString();
     return this;
   },
   once: (
     eventName: string | symbol,
     listener: (...args: any[]) => void,
   ): Dispatcher => {
-    eventName.toString();
-    listener.name.toString();
     return this;
   },
   removeListener: (
     eventName: string | symbol,
     listener: (...args: any[]) => void,
   ): Dispatcher => {
-    eventName.toString();
-    listener.name.toString();
     return this;
   },
   off: (
     eventName: string | symbol,
     listener: (...args: any[]) => void,
   ): Dispatcher => {
-    eventName.toString();
-    listener.name.toString();
     return this;
   },
   removeAllListeners: (event?: string | symbol): Dispatcher => {
-    event.toString();
     return this;
   },
   setMaxListeners: (n: number): Dispatcher => {
-    n.toString();
     return this;
   },
   getMaxListeners: (): number => {
     return 0;
   },
   listeners: (eventName: string | symbol): any[] => {
-    eventName.toString();
     return [this];
   },
   rawListeners: (eventName: string | symbol): any[] => {
-    eventName.toString();
     return [this];
   },
   emit: (eventName: string | symbol, ...args: any[]): boolean => {
-    eventName.toString();
-    args.toString();
     return true;
   },
   listenerCount: (eventName: string | symbol): number => {
-    eventName.toString();
     return 0;
   },
   prependListener: (
     eventName: string | symbol,
     listener: (...args: any[]) => void,
   ): Dispatcher => {
-    eventName.toString();
-    listener.name.toString();
     return this;
   },
   prependOnceListener: (
     eventName: string | symbol,
     listener: (...args: any[]) => void,
   ): Dispatcher => {
-    eventName.toString();
-    listener.name.toString();
     return this;
   },
   eventNames: (): (string | symbol)[] => {
